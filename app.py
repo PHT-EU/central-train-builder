c6f2b083833edffe34dccfe794edb34c4f9094b3
Lukas Zimmermann <luk.zim91@gmail.com>
2019-10-31 13:30:39 +0100

    Start Train Builder

---
+++ app.py
@@ -0,0 +1,100 @@
+import os
+from flask import Flask, request, abort, Response
+from flask_restplus import Resource, Api, Namespace
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+from pht.trainbuilder import Config
+
+__POST = 'POST'
+_BAD_REQUEST = 400
+_CREATED = 201
+
+app = Flask(__name__)
+api = Api(app)
+build = Namespace('build', description='Namespace for buiding trains')
+api.add_namespace(build)
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///build.sqlite"
+
+
+class Build(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String, unique=False, nullable=False)
+    filename = db.Column(db.String, unique=True, nullable=True)
+
+
+db.create_all()
+
+# TODO How to reconcile this with app.config ?
+config = Config.from_env()
+
+
+def bad_request():
+    abort(_BAD_REQUEST)
+
+
+@build.route('/upload')
+class BuildUpload(Resource):
+    @build.doc('Upload file to build service')
+    @build.response(_BAD_REQUEST, 'If the file could not be uploaded')
+    @build.response(_CREATED, 'If the file was uploaded successfully')
+    def post(self):
+        _file = 'file'
+        # check if the post request has the file part
+        if _file not in request.files:
+            bad_request()
+        file = request.files[_file]
+        if file.filename == '':
+            bad_request()
+        if file:
+            build = Build(type='file')
+            db.session.add(build)
+            db.session.commit()
+            filename = secure_filename(file.filename + f'_{build.id}')
+            build.filename = filename
+            db.session.merge(build)
+            db.session.commit()
+            file.save(os.path.join(config.upload_dir, filename))
+            return Response(status=201)
+        else:
+            bad_request()
+
+
+@build.route('/')
+class BuildPost(Resource):
+    @build.doc('Post build request')
+    @build.response(_BAD_REQUEST, 'If the build request is invalid')
+    @build.response(_CREATED, 'If the file was uploaded successfully')
+    def post(self):
+        if request.data:
+            data = request.data
+            build = Build(type='post')
+            db.session.add(build)
+            db.session.commit()
+            filename = f'_{build.id}'
+            build.filename = filename
+            db.session.merge(build)
+            db.session.commit()
+            with open(os.path.join(config.upload_dir, filename), 'wb') as f:
+                f.write(data)
+            return Response(status=201)
+        else:
+            bad_request()
+
+# @api.route('/<id>')
+# @api.param('id', 'The cat identifier')
+# @api.response(404, 'Cat not found')
+# class Cat(Resource):
+#     @api.doc('get_cat')
+#     @api.marshal_with(cat)
+#     def get(self, id):
+#         '''Fetch a cat given its identifier'''
+#         for cat in CATS:
+#             if cat['id'] == id:
+#                 return cat
+#         api.abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
