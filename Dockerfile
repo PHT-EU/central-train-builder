551bcb2ba9c206959ad65ac5438d7ab55870fe45
migraf <michael.graf3110@gmail.com>
2020-11-17 11:34:02 +0100

    updated logging

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY rsa.public /home/rsa.public
 COPY requirements.txt /home/requirements.txt
 COPY TrainBuilderService.py /home/TrainBuilderService.py
 
-RUN pip install -r /home/requirements.txt
+RUN pip install -r /home/requirements.txt && mkdir /home/build_dir
 
 CMD ["python", "-u", "/home/TrainBuilderService.py"]
--- TrainBuilderService.py
+++ TrainBuilderService.py
@@ -22,7 +22,8 @@ print(tb_dir)
 
 # Setup loggin and public key
 
-logging.basicConfig(filename=os.path.join(tb_dir, "train_builder.log"), level=logging.INFO)
+logging.basicConfig(filename=os.path.join(tb_dir, "train_builder.log"), level=logging.INFO,
+                    format='%(asctime)s %(message)s')
 
 with open(os.path.join(tb_dir, "rsa.public"), "r") as pk_file:
     pk = pk_file.read()
@@ -35,7 +36,7 @@ def validate_token(token):
     except jwt.ExpiredSignatureError:
         return False
     except BaseException as e:
-        err(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Token could not be authenticated\n {e}")
+        err(f"Token could not be authenticated\n {e}")
         return False
 
 
@@ -48,7 +49,7 @@ async def index(request):
 async def connect(sid, environ):
     print("connect event called")
     # TODO validate token
-    logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Client {sid} connected")
+    logging.info(f"Client {sid} connected")
     print(sid)
     await sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
 
@@ -58,54 +59,47 @@ async def build_train(sid, message):
     # Validate the token sent by the frontend
     token = message["token"]
     if not validate_token(token):
-        logging.warning(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Unauthorized login attempt {sid}")
+        logging.warning(f"Unauthorized login attempt {sid}")
         return {"success": False, "msg": "Unauthorized"}, 401
 
     if message["action"] == 'build':
         print("Building train")
         data = message["data"]
-        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Received build request, train id:"
-                     f" {data['train_id']}")
+        logging.info(f"Received build request, train id: {data['train_id']}")
         # Extract variables
         train_id = data["train_id"]
         route = data["route"]
         route = [str(x) for x in route]  # TR requires strings as harborProjects
 
-        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Posting route to vault:\nid: "
-                     f"{data['train_id']}")
+        logging.info(f"Posting route to vault:\nid: {data['train_id']}")
         # Add the route to vault to storage for processing by the train router
         try:
             post_route_to_vault(train_id, route)
         except ValueError as error:
-            err(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Error adding route to vault"
-                          f" {data['train_id']}:\n {error}")
+            err(f"Error adding route to vault {data['train_id']}:\n {error}")
 
             return {"success": False, "msg": "Route could not be added to vault"}, 300
-        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Building the train, id: "
-                     f"{data['train_id']}")
+        logging.info(f"Building the train, id: {data['train_id']}")
 
         msg = tb.build_train(data)
         if msg["success"]:
-            logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Successfully built train: id "
-                         f"{data['train_id']}")
+            logging.info(f"Successfully built train: id {data['train_id']}")
             print(msg)
             return msg, 200
         else:
-            err(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Error during build process of train"
-                          f" {data['train_id']}:\n {msg}")
+            err(f"Error during build process of train {data['train_id']}:\n {msg}")
             return msg, 300
 
     elif message["action"] == 'generateHash':
         print("Generating Hash")
         try:
             data = message["data"]
-            logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Generating Hash for train {data['train_id']}")
+            logging.info(f"Generating Hash for train {data['train_id']}")
 
             msg = tb.provide_hash(data)
             print(msg)
             if msg["success"]:
-                err(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Successfully generated hash for train"
-                             f" {data['train_id']}:\n Hash: {msg}")
+                logging.info(f"Successfully generated hash for train {data['train_id']}:\n Hash: {msg}")
                 print(msg)
                 return msg, 200
             else:
@@ -116,7 +110,7 @@ async def build_train(sid, message):
         except BaseException as e:
             # shutil.rmtree("pht_train")
             print(e)
-            e(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Something broke: \n{e}")
+            err(f"Something broke: \n{e}")
             return {"success": False, "msg": "Hash generation failure"}, 300
 
     # Validate a user submitted docker image and generate the security values for it
